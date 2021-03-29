ee71cd778215da22e51b44d4911dc4ef2092985f
blen_meku <mekuriablen@gmail.com>
2021-03-29 00:54:11 -0700

    modified

--- src/components/DietItem.js
+++ src/components/DietItem.js
@@ -5,21 +5,22 @@ import { deleteDiet } from "../actions/deleteDiet";
 
 const DietItem = ({ diet, deleteDiet }) => {
   return (
-    //  <div class="col-sm-6">
-    //     <div class="card">
-    //       <div class="card-body">
-    //         <h5 class="card-title">Special title treatment</h5>
-    //         <p class="card-text">
-    //           With supporting text below as a natural lead-in to additional
-    //           content.
-    //         </p>
-    //         <a href="#" class="btn btn-primary">
-    //           Go somewhere
-    //         </a>
+    // <div class="row">
+    //     <div class="col-sm-6">
+    //       <div class="card">
+    //         <div class="card-body">
+    //           <h5 class="card-title">Special title treatment</h5>
+    //           <p class="card-text">
+    //             With supporting text below as a natural lead-in to additional
+    //             content.
+    //           </p>
+    //           <a href="#" class="btn btn-primary">
+    //             Go somewhere
+    //           </a>
+    //         </div>
     //       </div>
     //     </div>
-    //   </div>
-    // </div>
+    //     <div>
 
     <div class="col-sm-6">
       <div className="card">
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,12 @@ import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="row ">
-      <div className="col s12 m7">
-        <div className="card large deep-purple lighten-5 ">
-          <div className="card-image">
-            <div className="card-panel deep-purple lighten-5 ">
-              <h1 className="center h4">Dieting...</h1>
-            </div>
-          </div>
-          <div className="card-content ">
-            <p>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">
               Welcome to HEALTHY MEALS! Diet is established among the most
               important influences on health in modern societies. Injudicious
               diet figures among the leading causes of premature death and
@@ -20,13 +16,59 @@ function Home() {
               expectancy, dramatic reduction in lifetime risk of all chronic
               disease, and amelioration of gene expression. In this context,
               claims abound for the competitive merits of various diets relative
-              to one another.
+              to one another. Welcome to HEALTHY MEALS! Diet is established
+              among the most important influences on health in modern societies.
+              Injudicious diet figures among the leading causes of premature
+              death and chronic disease. Optimal eating is associated with
+              increased life expectancy, dramatic reduction in lifetime risk of
+              all chronic disease, and amelioration of gene expression. In this
+              context, claims abound for the competitive merits of various diets
+              relative to one another. Welcome to HEALTHY MEALS! Diet is
+              established among the most important influences on health in
+              modern societies. Injudicious diet figures among the leading
+              causes of premature death and chronic disease. Optimal eating is
+              associated with increased life expectancy, dramatic reduction in
+              lifetime risk of all chronic disease, and amelioration of gene
+              expression. In this context, claims abound for the competitive
+              merits of various diets relative to one another.
             </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
           </div>
-          <div className="card-action">
-            <NavLink to="/diets">
-              Not Sure Where to start? Check Out these Diet Plans
-            </NavLink>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">
+              Welcome to HEALTHY MEALS! Diet is established among the most
+              important influences on health in modern societies. Injudicious
+              diet figures among the leading causes of premature death and
+              chronic disease. Optimal eating is associated with increased life
+              expectancy, dramatic reduction in lifetime risk of all chronic
+              disease, and amelioration of gene expression. In this context,
+              claims abound for the competitive merits of various diets relative
+              to one another. Welcome to HEALTHY MEALS! Diet is established
+              among the most important influences on health in modern societies.
+              Injudicious diet figures among the leading causes of premature
+              death and chronic disease. Optimal eating is associated with
+              increased life expectancy, dramatic reduction in lifetime risk of
+              all chronic disease, and amelioration of gene expression. In this
+              context, claims abound for the competitive merits of various diets
+              relative to one another. Welcome to HEALTHY MEALS! Diet is
+              established among the most important influences on health in
+              modern societies. Injudicious diet figures among the leading
+              causes of premature death and chronic disease. Optimal eating is
+              associated with increased life expectancy, dramatic reduction in
+              lifetime risk of all chronic disease, and amelioration of gene
+              expression. In this context, claims abound for the competitive
+              merits of various diets relative to one another.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
           </div>
         </div>
       </div>
